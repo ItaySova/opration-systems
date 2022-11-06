d83981b8e26cc84870ba7fada7a889bb4d86f5ea
Itay <95419130+ItaySova@users.noreply.github.com>
2022-11-06 11:27:42 +0200

    Add files via upload

---
+++ ex4/ex4_client.c
@@ -0,0 +1,201 @@
+// Itay Sova 31326069
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <time.h>
+
+// this is the client side
+
+char self_pid[100] = {'\0'};
+
+// function decleration:
+int find_txt_file(char* file_name);
+void read_ans_file(int conf_fd);
+void write_query_to_srv(char* srv_pid, char* first_num, char* operation, char* sec_num);
+void signal_hand (int sig);
+int input_check(int argc,char **argv);
+
+// function for finding if a file exist in current dir
+// if so, it returns 1. otherwise 0
+int find_txt_file(char* file_name){
+    DIR *pDir;
+    struct dirent *pDirent;
+    if ( (pDir = opendir(".")) == NULL){
+        //perror("opendir failed");
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    while ( (pDirent = readdir(pDir) ) != NULL ) {
+        if(strcmp(pDirent->d_name,file_name) == 0){
+            if(closedir(pDir) < 0){
+                //perror("Error in: close");
+                printf("ERROR_FROM_EX4\n");
+                exit(-1);
+            }
+            return 1;
+        }
+    }
+    if(closedir(pDir) < 0){
+        printf("ERROR_FROM_EX4\n");
+        //perror("Error in: close");
+        exit(-1);
+    }
+    return 0;
+}
+// function for reading the file returned from the server:
+void read_ans_file(int conf_fd) {
+    char buffer[101]={'\0'};
+    char* answer;
+    int bytes_read_from_conf;
+    bytes_read_from_conf = read(conf_fd,buffer,100);
+    if(bytes_read_from_conf == -1) {
+//        perror("Error in: read");
+        printf("ERROR_FROM_EX4\n");
+        close(conf_fd);
+        exit(-1);
+    }
+    answer = buffer;
+    if (strcmp(answer,"CANNOT_DIVIDE_BY_ZERO\n") == 0){
+        printf("CANNOT_DIVIDE_BY_ZERO\n");
+        close(conf_fd);
+    } else {
+        printf("%s\n",answer);
+        close(conf_fd);
+    }
+}
+
+void write_query_to_srv(char* srv_pid, char* first_num, char* operation, char* sec_num){
+    int query_fd = open("to_srv.txt", O_RDWR|O_CREAT|O_TRUNC|O_APPEND, 0777);
+    int self_int_pid = getpid();
+    char pid_for_file[100];
+    sprintf(pid_for_file, "%d", self_int_pid);
+    strcat(self_pid, pid_for_file);
+    write(query_fd,self_pid,strlen(self_pid));
+    write(query_fd,"\n",strlen("\n"));
+    write(query_fd,first_num,strlen(first_num));
+    write(query_fd,"\n",strlen("\n"));
+    write(query_fd,operation,strlen(operation));
+    write(query_fd,"\n",strlen("\n"));
+    write(query_fd,sec_num,strlen(sec_num));
+    close(query_fd);
+    int pid_srv = atoi(srv_pid);
+    if (kill(pid_srv,SIGUSR1) != 0){
+        // if the client cant signal the server exit with error
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+}
+
+
+void alarm_hand (int sig)
+{
+    signal(SIGALRM, alarm_hand);
+    printf("\nClient closed because no response was received from the server for 30 seconds\n");
+    exit(-1);
+}
+// signal for the answer from the server
+void signal_hand (int sig)
+{
+    signal(SIGUSR1, signal_hand);
+    // getting the pid of self
+    char ans_file_name[100] = {'\0'};
+    strcat(ans_file_name,"to_client_");
+    strcat(ans_file_name,self_pid);
+    strcat(ans_file_name, ".txt");
+    int answer_fd = open(ans_file_name, O_RDONLY);
+    if (answer_fd < 0){
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    read_ans_file(answer_fd);
+    // delete client ans file
+    if(remove(ans_file_name) == -1){
+        printf("ERROR_FROM_EX4\n");
+//        perror("Error in: remove");
+        exit(-1);
+    }
+    exit(0);
+}
+
+// input check return 1 if the input is valid and 0 if not
+int input_check(int argc,char **argv){
+    // checking all the input to be int type numbers:
+    int arg_index = 1;
+    // for loop for all args
+    for (;arg_index < argc ; arg_index++){
+        char input[100] = "";
+        int length,i;
+        strcpy(input, argv[arg_index]);
+        length = strlen (input);
+        // check if the number is 0
+        if (atoi(input)==0){
+            // if the input in atoi is 0 check if it is the number 0 or that its not a number at all
+            if (input[0] == '0'){
+                // continue to the next arg
+                continue;
+            } else {
+                // if atoi(input)==0 and input[0] != '0' then the arg is not a number
+                return 0;
+            }
+        }
+        for (i=0;i<length; i++){
+            //
+            if (!isdigit(input[i])){
+                if (i == 0 && input[i] == '-' ){
+                    // if the 1st char is not a digit it may be a "-"
+                    continue;
+                }
+                return 0;
+            }
+        }
+    }
+    // checking that p3 to be an operator between 1-4:
+    int operator_i = atoi(argv[3]);
+    if (operator_i > 4 || operator_i < 1){
+        return 0;
+    }
+    return 1;
+}
+
+
+int main(int argc,char **argv) {
+    if(argc < 5){
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    // input check
+    if (input_check(argc,argv) != 1){
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    // time for random
+    srand(time(NULL));
+    int time_to_wait;
+    // declaring signals:
+    signal(SIGUSR1, signal_hand);
+    signal(SIGALRM, alarm_hand);
+    // check if the to_srv file exist and if so wait random num of seconds and check again
+    // after 10 times it print error massage and exit
+    int num_of_times_in_loop = 0;
+    while (find_txt_file("to_srv.txt") == 1){
+        num_of_times_in_loop +=1;
+        time_to_wait = rand() % 6;
+        sleep(time_to_wait);
+        if (num_of_times_in_loop == 10){
+            printf("ERROR_FROM_EX4\n");
+            exit(-1);
+        }
+    }
+    write_query_to_srv(argv[1],argv[2],argv[3],argv[4]);
+    alarm(30); // alarm for 30 sec for serv answer
+    pause(); // pause until ans file is ready
+    return 0;
+}
---
+++ ex4/ex4_srv.c
@@ -0,0 +1,180 @@
+// Itay Sova 31326069
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/wait.h>
+// global pid
+pid_t glob_pid;
+// the server side:
+//global name for the query file name:
+char* name_or_query_file = "to_srv.txt";
+// function declaration:
+void activate_child(pid_t *pid_c);
+int compute_query(int first_num, int operation, int sec_num, char* answer_buff);
+void print_ans_to_file(int pid, char* ans);
+void read_query_file();
+void signal_hand (int sig);
+void alarm_hand (int sig);
+void manual_end_hand (int sig);
+void activate_child(pid_t *pid_c);
+
+// functions implementation:
+void activate_child(pid_t *pid_c){
+    if((*pid_c = fork()) == -1){
+        printf("ERROR_FROM_EX4\n");
+    } else if(*pid_c == 0 ){
+        //child process
+        // giving the entire service
+        read_query_file();
+        //killing child process
+        raise(SIGUSR2);
+    }
+}
+
+int compute_query(int first_num, int operation, int sec_num, char* answer_buff){
+    int answer;
+    switch (operation) {
+        case 1:
+            answer = first_num + sec_num;
+            break;
+        case 2:
+            answer = first_num - sec_num;
+            break;
+        case 3:
+            answer = first_num * sec_num;
+            break;
+        case 4:
+            if (sec_num == 0){
+                strcat(answer_buff,"CANNOT_DIVIDE_BY_ZERO\n");
+                return -1;
+            }
+            answer = first_num / sec_num;
+            break;
+    }
+    char final_ans[100];
+    sprintf(final_ans, "%d", answer);
+    strcat(answer_buff, final_ans);
+    return 0;
+}
+
+void print_ans_to_file(int pid, char* ans){
+    char pid_in_char[50] = {'\0'};
+    sprintf(pid_in_char, "%d", pid);
+    char new_ans_file_name[100] = {'\0'};
+    strcat(new_ans_file_name,"to_client_");
+    strcat(new_ans_file_name,pid_in_char);
+    strcat(new_ans_file_name,".txt");
+    int ans_fd = open(new_ans_file_name, O_RDWR|O_CREAT|O_TRUNC|O_APPEND, 0777);
+    write(ans_fd,ans,strlen(ans));
+    close(ans_fd);
+}
+// the function of the server which does the reading of the srv file, computation and writing the answer
+// to the client (when getting a signal from the client that func will be activated in fork)
+void read_query_file() {
+    char buffer[2] = {'\0'};
+    int line = 1;
+    char ans[100] = {'\0'};
+    int query_fd = open("to_srv.txt", O_RDWR);
+    if (query_fd < 0) {
+//        perror("Error in: open");
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    int bytes_read_from_query = read(query_fd, buffer, 1);
+    if (bytes_read_from_query < 0) {
+//        perror("Error in: read");
+        printf("ERROR_FROM_EX4\n");
+        close(query_fd);
+        exit(-1);
+    }
+    char client_pid[100] = {'\0'};
+    char first_num[100] = {'\0'};
+    char operation[100] = {'\0'};
+    char sec_num[100] = {'\0'};
+    while (bytes_read_from_query != 0) {
+        // if the current char is white space go to next line
+        if (isspace(buffer[0]) != 0) {
+            line += 1;
+        } else {
+            if (line == 1) {
+                strcat(client_pid, buffer);
+            } else if (line == 2) {
+                strcat(first_num, buffer);
+            } else if (line == 3) {
+                strcat(operation, buffer);
+            } else if (line == 4) {
+                strcat(sec_num, buffer);
+            }
+        }
+        // read the next byte
+        bytes_read_from_query = read(query_fd, buffer, 1);
+        if (bytes_read_from_query == -1) {
+//            perror("Error in: read");
+            printf("ERROR_FROM_EX4\n");
+            close(query_fd);
+            exit(-1);
+        }
+    }
+    close(query_fd);
+    // delete to_srv
+    if(remove("to_srv.txt") == -1){
+//        perror("Error in: remove");
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+    // convert to ints:
+    int pid = atoi(client_pid);
+    int number_1 = atoi(first_num);
+    int number_2 = atoi(sec_num);
+    int oper = atoi(operation);
+    // compute the query and write it into a file
+    compute_query(number_1,oper,number_2, ans);
+    print_ans_to_file(pid,ans);
+    // signal for the client to notify ans is ready
+    if (kill(pid,SIGUSR1) != 0){
+//      if failed to send signal to client - exit
+        printf("ERROR_FROM_EX4\n");
+        exit(-1);
+    }
+}
+
+void signal_hand (int sig)
+{
+    signal(SIGUSR1, signal_hand);
+    // function that fork a child and give service to the client
+    activate_child(&glob_pid);
+}
+
+void alarm_hand (int sig)
+{
+    signal(SIGALRM, alarm_hand);
+    printf("The server closed because no service request was received for the last 60 seconds\n");
+    while (wait(NULL) != -1);
+    exit(-1);
+}
+
+// signal for manually killing the child
+void manual_end_hand (int sig)
+{
+    signal(SIGUSR2, manual_end_hand);
+    while (wait(NULL) != -1);
+    exit(-1);
+}
+
+int main() {
+    signal(SIGUSR1, signal_hand);
+    signal(SIGALRM, alarm_hand);
+    signal(SIGUSR2, manual_end_hand);
+    int num_of_clients_max = 10, num_of_clients_so_far = 0;
+    while(1){
+        alarm(60);
+        pause();
+    }
+    while (wait(NULL) != -1);
+    return 0;
+}
